2feb07608e8d074352b61cfa3163877c42a33317
Luke Rokop <lrokop14@gmail.com>
2018-07-10 18:27:51 +0000

    added strikethrough

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-console.log("Hello World");
-/*
-$(document.ready( function(){
-    $("ul").on("click", input[type=checkbox], function(){
-        $(this).closest("li").toggleClass("completed-todo")
-    })
-})
-*/
-var nameoftodo=$("#add-to-do-button");
 $(document).ready(function(){
     console.log("the ship has landed")
     $("#add-to-do-button").click(function(){
         console.log("this Works")
             var NameOfTodo=$("#Add-todo-textbox").val()
-            console.log*(nameoftodo)
               var lengthOfTodo=$("#add-length-textbox").val()
             $("ul").append("<li><input type='checkbox'/>"+NameOfTodo+"<span class= 'time-estimate'> "+lengthOfTodo+" minutes</span></li>")
   
@@ -20,5 +10,8 @@ $(document).ready(function(){
             console.log*(lengthOfTodo)
         
     })
-
+   
+    $("input[type='checkbox']").click(function(){
+        $(this).closest("li").toggleClass("completed-todo")
+    })
 })
